7aa72b63a726e5d8c9e56f4a2e4da966c14ca1c2
24149039-art <24149039@drngpit.ac.in>
2026-03-10 14:59:26 +0530

    Create 134-GasStation

---
+++ 134-GasStation/134-GasStation.java
@@ -0,0 +1,16 @@
+// Last updated: 3/10/2026, 3:00:09 PM
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0, tank = 0, start = 0;
+        for (int i = 0; i < gas.length; i++) {
+            int diff = gas[i] - cost[i];
+            total += diff;
+            tank += diff;
+            if (tank < 0) {
+                tank = 0;
+                start = i + 1;
+            }
+        }
+        return total >= 0 ? start : -1;
+    }
+}
