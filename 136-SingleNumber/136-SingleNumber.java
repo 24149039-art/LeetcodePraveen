e80862d2fe1c7c186e90c648a73cb79b84d0c978
24149039-art <24149039@drngpit.ac.in>
2026-03-10 14:59:25 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.java
@@ -0,0 +1,10 @@
+// Last updated: 3/10/2026, 3:00:08 PM
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
