54938640eec0882f4d47bb309c73cce90d3c6429
24149039-art <24149039@drngpit.ac.in>
2026-03-10 14:59:06 +0530

    Create 693-BinaryNumberWithAlternatingBits

---
+++ 693-BinaryNumberWithAlternatingBits/693-BinaryNumberWithAlternatingBits.java
@@ -0,0 +1,9 @@
+// Last updated: 3/10/2026, 2:59:49 PM
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int x = n ^ (n >> 1);
+        return (x & (x + 1)) == 0;
+    }
+}
+
+
