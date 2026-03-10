9205bc4e744a44b77618c66b0e2959c04d3ba39a
24149039-art <24149039@drngpit.ac.in>
2026-03-10 14:59:05 +0530

    Create 782-JewelsAndStones

---
+++ 782-JewelsAndStones/782-JewelsAndStones.java
@@ -0,0 +1,12 @@
+// Last updated: 3/10/2026, 2:59:48 PM
+public class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            if (jewels.indexOf(stones.charAt(i)) != -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
